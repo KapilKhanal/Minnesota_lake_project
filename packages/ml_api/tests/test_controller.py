db835d63fe64e83913024e735b7818b9322bc41b
KapilKhanal <kkhanal16@winona.edu>
2019-12-29 11:54:16 -0600

    Imported

--- packages/ml_api/tests/test_controller.py
+++ packages/ml_api/tests/test_controller.py
@@ -1,5 +1,5 @@
 
-
+import lakeModel
 from lakeModel.config import config as model_config
 from lakeModel.processing.data_management import load_dataset
 from lakeModel import __version__ as _version
